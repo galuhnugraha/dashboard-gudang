9bbfb5acb08eea975a7ff27e1a1401e5e322996d
Galuh Nugraha <galuhnugraha28@gmail.com>
2021-02-01 22:34:17 +0700

    updates

--- src/pages/Payment/PurchaseOrder/AddPurchaseOrder/index.js
+++ src/pages/Payment/PurchaseOrder/AddPurchaseOrder/index.js
@@ -1,244 +1,236 @@
-// import React, { useEffect, useState } from "react";
-// import {
-//     Input,
-//     Form,
-//     message, Breadcrumb,
-//     PageHeader, Card, Button, Select
-// } from 'antd';
-// import { Link, useHistory } from 'react-router-dom';
-// import { useStore } from "../../../../utils/useStores";
-// import { observer } from "mobx-react-lite";
-
-
-// // function handleChange(value) {
-// //     console.log(`selected ${value}`);
-// // }
-
-// export const AddPurchaseOrder = observer(() => {
-//     var myItem = new Array()
-//     const store = useStore();
-//     const history = useHistory();
-//     const [loading, setLoading] = useState(false);
-//     const [item, setItem] = useState([]);
-//     const [form] = Form.useForm();
-//     useEffect(() => {
-//         fetchData();
-//         // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, []);
-
-//     function fetchData() {
-//         store.supliers.getSupplier();
-//         store.supliers.getSupplierProductReview();
-//     }
-
-//     const onFinish = values => {
-//         enterLoading(values);
-//     };
-
-//     const showItem = () => {
-//         const hero = store.supliers.detailData.map(r => {
-//             let item = {
-//                 productName: r.product?.productName,
-//                 quantity: r.product?.quantity,
-//                 selfing: r.product?.selfing,
-//                 rack: r.product?.rack
-//             }
-//             return item;
-//         })
-//         setItem(hero)
-//     }
-
-//     const enterLoading = (e) => {
-//         setLoading(true);
-//         const data = {
-//             purchaseName: e.purchaseName,
-//             pic: e.pic,
-//             item: myItem,
-//             // quantity: ''
-//             // quantity: dataTaro.push([item])
-//         }
-//         console.log(data)
-//         store.purchase.AddPurchaseOrder(data).then(res => {
-//             setLoading(false);
-//             message.success('Berhasil Add Product');
-//             history.push("/app/purchase-order");
-//         }).catch(err => {
-//             setLoading(false);
-//             message.error(err.message);
-//         });
-//     }
-
-
-//     return <div>
-//         <Breadcrumb>
-//             <Breadcrumb.Item>
-//                 {/* Home */}
-//                 <Link to={'/app/dashboard'}>Home</Link>
-//             </Breadcrumb.Item>
-//             <Breadcrumb.Item>
-//                 <Link to={'/app/purchase-order'}>Purchase Order</Link>
-//             </Breadcrumb.Item>
-//             <Breadcrumb.Item>
-//                 <span style={{ color: "#132743" }}>Input Purchase Order</span>
-//             </Breadcrumb.Item>
-//         </Breadcrumb>
-//         <Card
-//             bordered={false}
-//             className={"shadow"}
-//             bodyStyle={{ padding: 0, marginTop: 10, borderRadius: 5, boxShadow: '0 0 3px  0  rgba(0, 0, 0, 0.2), 0 3px 3px 0 rgba(0, 0, 0, 0.10)' }}
-//         >
-//             <PageHeader
-//                 className={"card-page-header"}
-//                 subTitle=""
-//                 title={"Input Purchase Order"}
-//             />
-//             <Form
-//                 layout={'vertical'}
-//                 name="normal_login"
-//                 className="login-form"
-//                 style={{ marginLeft: 23 }}
-//                 form={form}
-//                 onFinish={onFinish}
-//             >
-//                 <Form.Item
-//                     label="Purchase Name"
-//                     name="purchaseName"
-//                     size={'large'}
-//                     rules={[{ required: true, message: 'Please input your Product Name!' }]}
-//                 >
-//                     <Input style={{ width: '98%' }} />
-//                 </Form.Item>
-//                 <Form.Item
-//                     label="PIC"
-//                     name="pic"
-//                     size={'large'}
-//                     rules={[{ required: true, message: 'Please input your Product Type!' }]}
-//                 >
-//                     <Input style={{ width: '98%' }} />
-//                 </Form.Item>
-//                 <Form.Item
-//                     label="Product Item"
-//                     name="item"
-//                     size={'large'}
-//                 >
-//                     <Select
-//                         placeholder="Select Product"
-//                         style={{ width: '98%' }}
-//                         mode="default"
-//                         onChange={(value) => {
-//                             showItem(value)
-//                         }}
-//                     >
-//                         {store.supliers.data.map(d => <Select.Option value={d._id} key={d._id}>{d.suplierName}</Select.Option>)}
-//                     </Select>
-//                 </Form.Item>
-//                 {item.length > 0 &&  <p>Tes</p>}
-//                 <Form.Item
-//                     style={{
-//                         marginBottom: 25,
-//                         width: 100
-//                     }}>
-//                     <Button style={{ backgroundColor: '#132743', color: 'white', borderRadius: 5 }}
-//                         block
-//                         htmlType="submit"
-//                         size={'large'}
-//                         loading={loading}
-//                     >
-//                         Submit
-// 					</Button>
-//                 </Form.Item>
-//             </Form>
-//         </Card>
-
-//     </div>
-// })
-
-
-import React, { useEffect, useState } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import {
     Input,
     Form,
     message, Breadcrumb,
-    PageHeader, Card, Button, Select
+    PageHeader, Card, Button, Select, Popconfirm, Table
 } from 'antd';
 import { Link, useHistory } from 'react-router-dom';
 import { useStore } from "../../../../utils/useStores";
 import { observer } from "mobx-react-lite";
-import { FormInstance } from 'antd/lib/form';
 
 
 // function handleChange(value) {
 //     console.log(`selected ${value}`);
 // }
 
+const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+        <Form form={form} component={false}>
+            <EditableContext.Provider value={form}>
+                <tr {...props} />
+            </EditableContext.Provider>
+        </Form>
+    );
+};
+
+const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+}) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+    useEffect(() => {
+        if (editing) {
+            inputRef.current.focus();
+        }
+    }, [editing]);
+
+    const toggleEdit = () => {
+        setEditing(!editing);
+        form.setFieldsValue({
+            [dataIndex]: record[dataIndex],
+        });
+    };
+
+    const save = async () => {
+        try {
+            const values = await form.validateFields();
+            toggleEdit();
+            handleSave({ ...record, ...values });
+        } catch (errInfo) {
+            console.log('Save failed:', errInfo);
+        }
+    };
+
+    let childNode = children;
+
+    if (editable) {
+        childNode = editing ? (
+            <Form.Item
+                style={{
+                    margin: 0,
+                }}
+                name={dataIndex}
+                rules={[
+                    {
+                        required: true,
+                        message: `${title} is required.`,
+                    },
+                ]}
+            >
+                <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+            </Form.Item>
+        ) : (
+                <div
+                    className="editable-cell-value-wrap"
+                    style={{
+                        paddingRight: 24,
+                    }}
+                    onClick={toggleEdit}
+                >
+                    {children}
+                </div>
+            );
+    }
+
+    return <td {...restProps}>{childNode}</td>;
+};
+
+const EditableContext = React.createContext(null);
+
 export const AddPurchaseOrder = observer(() => {
     var myItem = new Array()
-    var newItem = ""
-    var newID = ""
     const store = useStore();
     const history = useHistory();
     const [loading, setLoading] = useState(false);
     const [item, setItem] = useState([]);
-    const [productId, setProductId] = useState('')
-    const [form] = Form.useForm();
-    const [quncy, setQuantity] = useState('')
-    const { Option } = Select;
     const [state, setState] = useState({
-        success: false,
-        item: '',
-        quantity: ''
-    });
+        dataSource: [
+            {
+                key: '0',
+                name: 'Edward King 0',
+                age: '32',
+                address: 'London, Park Lane no. 0',
+            },
+            {
+                key: '1',
+                name: 'Edward King 1',
+                age: '32',
+                address: 'London, Park Lane no. 1',
+            },
+        ],
+    })
+    const [form] = Form.useForm();
 
-    // console.log(productId)
     useEffect(() => {
         fetchData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    function fetchData() {
-        store.products.getAll();
-    }
-
-    const onFinish = values => {
-
-        enterLoading(values);
+    const components = {
+        body: {
+            row: EditableRow,
+            cell: EditableCell,
+        },
     };
 
-    const getQuantity = (val) => {
-        newItem = val.target.value
+    const tableData = [
+        {
+            title: 'name',
+            dataIndex: 'name',
+            width: '30%',
+            editable: true,
+        },
+        {
+            title: 'age',
+            dataIndex: 'age',
+        },
+        {
+            title: 'address',
+            dataIndex: 'address',
+        },
+        {
+            title: 'operation',
+            dataIndex: 'operation',
+            render: (_, record) =>
+                state.dataSource.length >= 1 ? (
+                    <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
+                        <a>Delete</a>
+                    </Popconfirm>
+                ) : null,
+        },
+    ];
+
+    const columns = tableData.map((col) => {
+        if (!col.editable) {
+            return col;
+        }
+
+        return {
+            ...col,
+            onCell: (record) => ({
+                record,
+                editable: col.editable,
+                dataIndex: col.dataIndex,
+                title: col.title,
+                handleSave: handleSave,
+            }),
+        };
+    });
+
+
+    function fetchData() {
+        store.supliers.getSupplier();
+        store.supliers.getSupplierProductReview();
     }
 
-    const getID = (val) => {
-        newID = val
+    const showItem = () => {
+        const hero = store.supliers.detailData.map(r => {
+            let item = {
+                productName: r.product?.productName,
+                quantity: r.product?.quantity,
+                selfing: r.product?.selfing,
+                rack: r.product?.rack
+            }
+            return item;
+        })
+        setItem(hero)
     }
-    const AddItem = () => {
-        const value = {
-            productId: newID,
-            quantity: newItem
-        }
-        myItem.push(value)
-        form.resetFields(["quantity", 'item', []]);
-        console.log(myItem)
-    }
-    const enterLoading = (e) => {
-        setLoading(true);
-        const data = {
-            purchaseName: e.purchaseName,
-            pic: e.pic,
-            item: myItem,
-            // quantity: ''
-            // quantity: dataTaro.push([item])
-        }
-        console.log(data)
-        store.purchase.AddPurchaseOrder(data).then(res => {
-            setLoading(false);
-            message.success('Berhasil Add Product');
-            history.push("/app/purchase-order");
-        }).catch(err => {
-            setLoading(false);
-            message.error(err.message);
+
+    const handleDelete = (key) => {
+        const dataSource = [...state.dataSource];
+        setState({
+            dataSource: dataSource.filter((item) => item.key !== key),
         });
-    }
+    };
+
+    const handleSave = (row) => {
+        const newData = [...state.dataSource];
+        const index = newData.findIndex((item) => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setState({
+            dataSource: newData,
+        });
+    };
+
+    // const enterLoading = (e) => {
+    //     setLoading(true);
+    //     const data = {
+    //         purchaseName: e.purchaseName,
+    //         pic: e.pic,
+    //         item: myItem,
+    //         // quantity: ''
+    //         // quantity: dataTaro.push([item])
+    //     }
+    //     console.log(data)
+    //     store.purchase.AddPurchaseOrder(data).then(res => {
+    //         setLoading(false);
+    //         message.success('Berhasil Add Product');
+    //         history.push("/app/purchase-order");
+    //     }).catch(err => {
+    //         setLoading(false);
+    //         message.error(err.message);
+    //     });
+    // }
 
 
     return <div>
@@ -263,19 +255,24 @@ export const AddPurchaseOrder = observer(() => {
                 className={"card-page-header"}
                 subTitle=""
                 title={"Input Purchase Order"}
+                extra={[
+                    <Button style={{ backgroundColor: '#132743', color: 'white', borderRadius: 5 }}
+                        block
+                        htmlType="submit"
+                        size={'large'}
+                        loading={loading}
+                    >
+                        Submit
+                </Button>
+                ]}
             />
-            {/* <Form  form={form} name="control-hooks" onFinish={onFinish}>
-                <Form.Item name="note" label="Note" rules={[{ required: true }]}>
-                    <Input />
-                </Form.Item>
-            </Form> */}
             <Form
                 layout={'vertical'}
                 name="normal_login"
                 className="login-form"
                 style={{ marginLeft: 23 }}
                 form={form}
-                onFinish={onFinish}
+            // onFinish={onFinish}
             >
                 <Form.Item
                     label="Purchase Name"
@@ -297,52 +294,34 @@ export const AddPurchaseOrder = observer(() => {
                     label="Product Item"
                     name="item"
                     size={'large'}
-                // rules={[{ required: true, message: 'Please input your Product Type!' }]}
                 >
                     <Select
                         placeholder="Select Product"
                         style={{ width: '98%' }}
                         mode="default"
                         onChange={(value) => {
-                            // setItem(value)
-                            // setProductId(value)
-                            getID(value)
-                            console.log(value)
-                            // AddItem(value)
+                            showItem(value)
                         }}
                     >
-                        {store.products.data.map(d => <Select.Option value={d._id} key={d._id}>{d.productName}</Select.Option>)}
+                        {store.supliers.data.map(d => <Select.Option value={d._id} key={d._id}>{d.suplierName}</Select.Option>)}
                     </Select>
                 </Form.Item>
-                <Form.Item
-                    label="Quantity"
-                    name="quantity"
-                    size={'large'}
-                // rules={[{ required: true, message: 'Please input your Product Type!' }]}
-
-                >
-                    <Input style={{ width: '98%' }} onChange={(value) => getQuantity(value)} />
-                </Form.Item>
+                {item.length > 0 && <Table
+                    components={components}
+                    rowClassName={() => 'editable-row'}
+                    bordered
+                    style={{ width: '98%' }}
+                    dataSource={state.dataSource}
+                    columns={columns}
+                />}
                 <Form.Item
                     style={{
                         marginBottom: 25,
                         width: 100
                     }}>
-                    <Button style={{ backgroundColor: '#132743', color: 'white', borderRadius: 5 }}
-                        block
-                        htmlType="submit"
-                        size={'large'}
-                        loading={loading}
-                    >
-                        Submit
-					</Button>
                 </Form.Item>
-                <Button onClick={AddItem}>add</Button>
             </Form>
-            <div>
-                {myItem.map((r) => { return (<div key={r.productName}><h5>{r.productName}</h5><br /><h5>{r.quantity}</h5></div>) })}
-            </div>
         </Card>
 
-    </div>
+    </div >
 })
